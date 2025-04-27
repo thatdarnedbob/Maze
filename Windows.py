407948705bea6e16616614d4d7a88ceae445e04d
thatdarnedbob <123yiaFTUTi121yiuhjk>
2025-04-27 12:29:10 -0500

    initial Hello World state

---
+++ Windows.py
@@ -0,0 +1,27 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+        self.__root = Tk()
+        self.__root.title = "Mazes"
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+        self.canvas = Canvas()
+        self.canvas.pack()
+
+        self.running = False
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+    
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+
+    def close(self):
+        self.running = False
