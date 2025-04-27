39eaf9a78543913335e80c90b468ff2470b2fc50
thatdarnedbob <123yiaFTUTi121yiuhjk>
2025-04-27 15:28:24 -0500

    Drawing moves, cell to cell

--- Geometry.py
+++ Geometry.py
@@ -12,3 +12,45 @@ class Line:
         canvas.create_line(self.begin.x, self.begin.y,
                            self.end.x, self.end.y,
                            fill=fill_c, width=2)
+
+class Cell:
+    def __init__(self, tl_corner, br_corner, window, walls=[True, True, True, True]):
+        self.walls = walls
+        # walls format: top, bottom, left, right
+        self._top_left_corner = tl_corner
+        self._bottom_right_corner = br_corner
+
+        self._top_right_corner = Point(self.x2(), self.y1())
+        self._bottom_left_corner = Point(self.x1(), self.y2())
+        self._win = window
+    
+    def x1(self):
+        return self._top_left_corner.x
+    def x2(self):
+        return self._bottom_right_corner.x
+    def y1(self):
+        return self._top_left_corner.y
+    def y2(self):
+        return self._bottom_right_corner.y
+
+    def draw(self):
+        if(self.walls[0]):
+            self._win.draw_line(Line(self._top_left_corner, self._top_right_corner), "black")
+        if(self.walls[1]):
+            self._win.draw_line(Line(self._bottom_left_corner, self._bottom_right_corner), "black")
+        if(self.walls[2]):
+            self._win.draw_line(Line(self._top_left_corner, self._bottom_left_corner), "black")
+        if(self.walls[3]):
+            self._win.draw_line(Line(self._top_right_corner, self._bottom_right_corner), "black")
+        return
+    
+    def draw_move(self, to_cell, undo=False):
+        if undo:
+            c = "gray"
+        else:
+            c = "red"
+        self._win.draw_line(Line(Point(0.5 * (self.x1() + self.x2()),
+                                       0.5 * (self.y1() + self.y2())),
+                                Point(0.5 * (to_cell.x1() + to_cell.x2()),
+                                       0.5 * (to_cell.y1() + to_cell.y2()))),
+                            c)
--- main.py
+++ main.py
@@ -1,16 +1,43 @@
 from Window import Window
-from Geometry import Point, Line
+from Geometry import *
 
 def main():
     win = Window(800, 600)
 
+    basic_cell_test(win)
+    
+    win.wait_for_close()
+
+def basic_cell_test(win):
+    A1, A2 = Point(10,10), Point(50,50)
+    Awalls = [True, True, True, True]
+
+    B1, B2 = Point(410,10), Point(450, 50)
+    Bwalls = [True, True, False, False]
+    
+    C1, C2 = Point(410,410), Point(450, 450)
+    Cwalls = [False, False, True, True]
+
+    D1, D2 = Point(10, 410), Point(50, 450)
+    Dwalls = [False, True, False, False]
+
+    Acell = Cell(A1, A2, win, Awalls)
+    Bcell = Cell(B1, B2, win, Bwalls)
+    Ccell = Cell(C1, C2, win, Cwalls)
+    Dcell = Cell(D1, D2, win, Dwalls)
+
+    for cell in [Acell, Bcell, Ccell, Dcell]:
+        cell.draw()
+    Bcell.draw_move(Ccell, False)
+    Ccell.draw_move(Dcell, True)
+
+def basic_line_test(win):
     L1 = Line(Point(0,0), Point(200,200))
     L2 = Line(Point(200,0), Point(0,200))
     L3 = Line(Point(200,0), Point(200,200))
     L4 = Line(Point(200,200), Point(0,200))
     for line in [L1, L2, L3, L4]:
         win.draw_line(line, "black")
-    win.wait_for_close()
 
 if __name__ == "__main__":
     main()
